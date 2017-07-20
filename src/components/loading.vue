5b42b72e26f2422bbb30b7fcfa42b0c75496f9a9
Gcalolin <418146654@qq.com>
2017-07-20 15:22:48 +0800

    加载时的样式

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -26,11 +26,12 @@
 		position: relative;
 		
 	}
+	/*这里的dispaly:inline-block非常重要,span是行内元素，没有高度，必须设置成块级元素*/
 	.loading span{
 		width: 1px;
 		height: 10px;
 		background: #f60;
-		display: inline-block;
+		display: inline-block; 
 		animation: loading 1.4s ease-out infinite;
 	}
 	.loading span:nth-child(2){
